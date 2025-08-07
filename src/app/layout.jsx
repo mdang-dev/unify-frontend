902b849ee4b656f08502c7f3b6d3e2829a269a06
anninh230705 <ninhnapc09333@gmail.com>
2025-08-07 14:34:42 +0000

    chore(tab): add icon and rename

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,10 @@
 import RootLayout from '../layouts/root-layout';
 
+export const metadata = {
+  title: 'Unify - Social Network',
+  description: 'Connect, Share, and Explore with Unify Social Network',
+};
+
 export default function Layout({ children }) {
   return <RootLayout>{children}</RootLayout>;
 }
