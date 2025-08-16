294fb26115949464b8c3b3861d4ba3c4f805a904
Tan Vinh <tanvinh06012005@gmail.com>
2025-08-16 18:19:29 +0700

    feat(supabase): implement supabase for storing media

--- src/configs/supabase.config.js
+++ src/configs/supabase.config.js
@@ -4,3 +4,11 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+const storageUrl = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL
+const storageAnonKey = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_ANON_KEY
+export const supabaseStorage = createClient(storageUrl, storageAnonKey)
+
+// Storage bucket name
+export const STORAGE_BUCKET = 'media' 
